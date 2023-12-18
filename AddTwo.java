8dc1d369137d40e89bf41b2953307684ece26745
Lidor Amraby <154251831+lidorStudent@users.noreply.github.com>
2023-12-18 20:05:01 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -4,7 +4,7 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		int args1 = Integer.parseInt(args[0]);
-        int args2 = Integer.parseInt(args[1]);
-        System.out.println(args1 + " + " + args2 + " = " + (args1 + args2));	
+        	int args2 = Integer.parseInt(args[1]);
+        	System.out.println(args1 + " + " + args2 + " = " + (args1 + args2));	
 	}
 }
