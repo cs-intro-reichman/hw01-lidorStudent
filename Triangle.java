c08ccbb409dc3b7d82eafa9c0bb3c3b5b1389e95
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-18 21:27:20 +0200

    Add awesome code for Homework 1

--- Triangle.java
+++ Triangle.java
@@ -8,5 +8,9 @@ public class Triangle {
 		int side1 = Integer.parseInt(args[0]);
         int side2 = Integer.parseInt(args[1]);
         int side3 = Integer.parseInt(args[2]);
+		boolean isTriangle = ((side1 + side2 > side3) && 
+							  (side1 + side3 > side2) && 
+							  (side2 + side3 > side1));
+		System.out.println(side1 + ", " + side2 + ", " + side3 + ": " + isTriangle);
 	}
 }
