9e06363c31e42d16312592f6dc15973577d79e10
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-18 22:07:03 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -6,5 +6,9 @@ public class GenThree {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
+		int random1 = (int)((Math.random() * (b - a)) + a);
+		int random2 = (int)((Math.random() * (b - a)) + a);
+		int random3 = (int)((Math.random() * (b - a)) + a);
+		
 	}
 }
